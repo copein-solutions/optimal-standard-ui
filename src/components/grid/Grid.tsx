86a5fe92a800dc2495c5e1956e3376a94886b42a
fausto137 <faustoperillo@gmail.com>
2023-07-12 20:41:53 -0300

    Corrige bug en pantalla system y agrega descripciones en Grid

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -14,8 +14,11 @@ type GridProps = {
   header?: Header[];
   /** Datos que se desplegaran en la lista. */
   body?: { id: number; [key: string]: any }[];
+  /** Si es true, se renderizará el botón de editar elemento. */
   hasEdit?: boolean;
+  /** Si es false, se renderizará el botón de eliminar elemento. */
   hasDelete?: boolean;
+  /** String que indica la url a la cual redireccionará el botón de edit. */
   editNav?: string;
 };
 
@@ -37,7 +40,7 @@ export const GridCustom: React.FC<GridProps> = ({
 
   const onEdit = (id: number) => {
     console.log("edit", id);
-    navigator(`/${editNav}/${id}/edit`)
+    navigator(`/${editNav}/${id}/edit`);
   };
 
   const editButton = (id: number) => (
