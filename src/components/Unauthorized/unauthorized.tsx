76a4a3c980f2fec9182eda805b015efe36025f53
Pablo Ingold <p.ingold@outlook.com>
2023-08-20 14:12:37 -0300

    on changes reviews

--- src/components/Unauthorized/unauthorized.tsx
+++ src/components/Unauthorized/unauthorized.tsx
@@ -1,31 +1,10 @@
-import React from 'react';
+import "./unauthorized.css";
 
 const Unauthorized: React.FC = () => {
-  const containerStyle: React.CSSProperties = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    minHeight: '100vh',
-    fontFamily: 'Arial, sans-serif',
-    textAlign: 'center',
-    background: '#f4f4f4',
-  };
-
-  const headingStyle: React.CSSProperties = {
-    fontSize: '3rem',
-    marginBottom: '1rem',
-  };
-
-  const messageStyle: React.CSSProperties = {
-    fontSize: '1.2rem',
-    color: '#666',
-  };
-
   return (
-    <div style={containerStyle}>
-      <h1 style={headingStyle}>404 - Página no encontrada</h1>
-      <p style={messageStyle}>Lo sentimos, la página que estás buscando no existe.</p>
+    <div className="containerStyle">
+      <h1 className="headingStyle">404 - Página no encontrada</h1>
+      <p className="messageStyle">Lo sentimos, la página que estás buscando no existe.</p>
     </div>
   );
 };
