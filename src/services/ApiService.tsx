be900a01af7332bf13765b1eaf54b2e9984ec380
manucleal <emanuel.coitino@mercadolibre.com>
2023-08-27 18:01:13 -0300

    creando variables de entorno segun el entorno de ejecución

--- src/services/ApiService.tsx
+++ src/services/ApiService.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { ResponseApi } from "../interfaces/service/ApiInterfaces";
 
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 let config: AxiosRequestConfig = {
   headers: {
     "Content-Type": "Application/json",
