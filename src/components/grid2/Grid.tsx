e147b9ab6b97cfc47828a20251490eb70c0ae9ad
fausto137 <faustoperillo@gmail.com>
2023-08-27 15:20:14 -0300

    definir stdo listo

--- src/components/grid2/Grid.tsx
+++ src/components/grid2/Grid.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Box,
   Button,
@@ -21,7 +21,7 @@ import {
   deleteMaterial,
   setSystemCategory,
 } from "../../services/ApiService";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Toast, { ToastType } from "../toast/toast";
 
 import {
@@ -34,6 +34,7 @@ import {
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import CustomSelectField from "../customSelectField";
+import { RootState } from "../../redux/reducers/reducer";
 
 type Header = {
   name: string;
@@ -85,6 +86,8 @@ export const GridCustom: React.FC<GridProps> = ({
   const [toastMsg, setToastMsg] = useState("");
   const [toastType, setToastType] = useState<ToastType>("success");
   const [selectedOption, setSelectedOption] = useState("");
+
+  const systems = useSelector((state: RootState) => state.systems);
 
   const onDelete = async () => {
     let response: any;
@@ -194,23 +197,38 @@ export const GridCustom: React.FC<GridProps> = ({
 
   const closeCategoryModal = () => {
     setSystemItemId(null);
-    setApplicationAreaName('');
-    setSelectedOption('');
+    setApplicationAreaName("");
+    setSelectedOption("");
     setModalWithChildrenOpen(false);
   };
 
   const handleConfirmCategorization = async () => {
     setModalWithChildrenOpen(false);
     const type = { type: selectedOption };
+    console.log(type);
+
     if (systemItemId) {
       const response: any = await setSystemCategory(systemItemId, type);
       if (response.status !== 200) {
         handleOpenToast(
-          "Algo salio mal al definir el estandar optimo.",
+          "Algo salio mal al definir el estándar óptimo.",
           "error"
         );
       } else {
-        handleOpenToast("Nuevo estandar optimo definido.", "success");
+        if (selectedOption === "OPTIMAL_STANDARD") {
+          dispatch({ type: "SET_OPTIMAL_STANDARD", payload: systemItemId });
+          handleOpenToast("Nuevo estándar optimo definido.", "success");
+        }
+        if (selectedOption === "ALTERNATIVE_OPTIMAL_STANDARD") {
+          dispatch({
+            type: "SET_ALTERNATIVE_OPTIMAL_STANDARD",
+            payload: systemItemId,
+          });
+          handleOpenToast(
+            "Nuevo estándar óptimo alternativo definido.",
+            "success"
+          );
+        }
       }
     }
     setSelectedOption("");
@@ -243,6 +261,7 @@ export const GridCustom: React.FC<GridProps> = ({
     header?.forEach((col) => {
       rowData[col.value] = item[col.value];
     });
+
     if (hasEdit || hasDelete) {
       rowData.actions = (
         <>
@@ -254,6 +273,38 @@ export const GridCustom: React.FC<GridProps> = ({
     }
     formatRows.push(rowData);
   });
+
+  const getRowClassName = (params: any) => {
+    const otimalStandardIds: number[] = [];
+    const alternativeOtimalStandardIds: number[] = [];
+
+    for (const objeto of systems) {
+      if (objeto.systemCategory === "OPTIMAL_STANDARD") {
+        otimalStandardIds.push(objeto.id);
+      }
+      if (objeto.systemCategory === "ALTERNATIVE_OPTIMAL_STANDARD") {
+        alternativeOtimalStandardIds.push(objeto.id);
+      }
+    }
+
+    if (otimalStandardIds.includes(params.row.id)) {
+      return "optimal-standard-color";
+    }
+    if (alternativeOtimalStandardIds.includes(params.row.id)) {
+      return "alternative-optimal-standard-color";
+    }
+
+    // if (params.row?.systemCategory === "OPTIMAL_STANDARD") {
+    //   return "optimal-standard-color";
+    // }
+    // if (params.row?.systemCategory === "ALTERNATIVE_OPTIMAL_STANDARD") {
+    //   return "alternative-optimal-standard-color";
+    // }
+    // if (params.row?.systemCategory === null) {
+    //   return "null";
+    // }
+    return "";
+  };
 
   const columns: GridColDef[] =
     header?.map((col) => ({
@@ -290,6 +341,7 @@ export const GridCustom: React.FC<GridProps> = ({
   return (
     <div className="data-grid-wrapper">
       <DataGrid
+        getRowClassName={getRowClassName}
         rows={formatRows}
         columns={columns}
         slots={{
@@ -323,7 +375,7 @@ export const GridCustom: React.FC<GridProps> = ({
           open={modalWithChildrenOpen}
           onClose={closeCategoryModal}
           title="Seleccione la categorización del sistema"
-          description={`Campo de aplicacion: ${applicationAreaName}`}
+          description={`Campo de aplicación: ${applicationAreaName}`}
           children={modalChildren}
           onConfirm={() => {
             handleConfirmCategorization();
--- src/pages/system/listSystem/listSystem.tsx
+++ src/pages/system/listSystem/listSystem.tsx
@@ -26,6 +26,7 @@ const ListSystem = () => {
         let listSystems: any = response.data;
         listSystems.map(
           (system: {
+            id: any;
             applicationArea: any;
             materials: any;
             cured: any;
@@ -58,6 +59,7 @@ const ListSystem = () => {
             materialPotLife: any;
             materialMinApplicableTemp: any;
           }) => {
+            system.id = system.id;
             system.applicationAreaName = system.applicationArea?.name;
             system.cured = system.cured ? "Si" : "No";
             system.totalPrice = `${system.totalPrice.toFixed(2)} $/m2`;
@@ -155,25 +157,37 @@ const ListSystem = () => {
 
   const userRole = localStorage.getItem("userRole");
 
+  const colorExplanation = (
+    <div className="color-explanation">
+      <text>Estándar óptimo</text>
+      <div className="optimal-standard"></div>
+      <text>Estándar óptimo alternativo</text>
+      <div className="alternative-optimal-standard"></div>
+    </div>
+  );
+
   return (
-    <MainContainer cardTitle="Sistema">
-      <div>
-        {userRole === ADMIN_ROL && (
-          <Button variant="text" color="success" onClick={handleAddSystem}>
-            Agregar sistema
-          </Button>
-        )}
-        <GridCustom
-          header={header}
-          body={systems}
-          hasEdit={userRole === ADMIN_ROL}
-          hasDelete={userRole === ADMIN_ROL}
-          hasCategory={userRole === ADMIN_ROL}
-          navigateTo="system"
-          columnGroupingModel={columnGroupingModel}
-        />
-      </div>
-    </MainContainer>
+    <>
+      <MainContainer cardTitle="Sistemas">
+        <div>{colorExplanation}</div>
+        <div>
+          {userRole === ADMIN_ROL && (
+            <Button variant="text" color="success" onClick={handleAddSystem}>
+              Agregar sistema
+            </Button>
+          )}
+          <GridCustom
+            columnGroupingModel={columnGroupingModel}
+            header={header}
+            body={systems}
+            hasEdit={userRole === ADMIN_ROL}
+            hasDelete={userRole === ADMIN_ROL}
+            hasCategory={userRole === ADMIN_ROL}
+            navigateTo="system"
+          />
+        </div>
+      </MainContainer>
+    </>
   );
 };
 
